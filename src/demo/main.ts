bb16cfbe16b9dce20d5b5926414ade56673212e9
John Reilly <johnny_reilly@hotmail.com>
2015-09-12 22:52:46 +0100

    tweak

--- src/demo/main.ts
+++ src/demo/main.ts
@@ -1,5 +1,5 @@
-//import './dependencies';
 import * as React from 'react';
+import './dependencies';
 import App from './components/App';
 
 React.render(React.createElement("App"), document.getElementById('content'));
