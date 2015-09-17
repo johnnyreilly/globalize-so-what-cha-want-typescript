798f65febd5fd9e61f24dc990211b5c5193adecf
John Reilly <johnny_reilly@hotmail.com>
2015-09-17 06:15:45 +0100

    no test results should fail the build

--- registerTestResults.ps1
+++ registerTestResults.ps1
@@ -3,25 +3,31 @@ write-host "Present working directory: $($pwd)"
 $testsuites = [xml](get-content .\test-results\*.xml)
 $anyFailures = $FALSE
 
-foreach ($testsuite in $testsuites.testsuite) {
+if ($testsuites) {
 
-    write-host " $($testsuite.name)"
+    foreach ($testsuite in $testsuites.testsuite) {
 
-    foreach ($testcase in $testsuite.testcase){
-        $failed = $testcase.failure
-        $time = [decimal]$testcase.time * 1000 # from seconds to milliseconds
-        $testName = "$($testcase.classname) $($testcase.name)"
+        write-host " $($testsuite.name)"
 
-        if ($failed) {
-            write-host "Failed $($testcase.name) $($testcase.failure.message) in $($time) ms"
-            Add-AppveyorTest $testName -Outcome Failed -FileName $testsuite.name -ErrorMessage $testcase.failure.message -Duration $time
-            $anyFailures = $TRUE
-        }
-        else {
-            write-host "Passed $($testcase.name) in $($time) ms"
-            Add-AppveyorTest $testName -Outcome Passed -FileName $testsuite.name -Duration $time
+        foreach ($testcase in $testsuite.testcase){
+            $failed = $testcase.failure
+            $time = [decimal]$testcase.time * 1000 # from seconds to milliseconds
+            $testName = "$($testcase.classname) $($testcase.name)"
+
+            if ($failed) {
+                write-host "Failed $($testcase.name) $($testcase.failure.message) in $($time) ms"
+                Add-AppveyorTest $testName -Outcome Failed -FileName $testsuite.name -ErrorMessage $testcase.failure.message -Duration $time
+                $anyFailures = $TRUE
+            }
+            else {
+                write-host "Passed $($testcase.name) in $($time) ms"
+                Add-AppveyorTest $testName -Outcome Passed -FileName $testsuite.name -Duration $time
+            }
         }
     }
+}
+else {
+    $anyFailues = $TRUE # no test results is a failure
 }
 
 if ($anyFailures -eq $TRUE){
